889a65a7bb0e6332b86d5ed6f20f4310e94bcffc
patsancu <patricksanzo@gmail.com>
2025-06-21 12:12:10 +0200

    feat: React template with tailwind

---
+++ README.md
@@ -0,0 +1,9 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with tailwind and (TODO) progressive web app config
+
+* Steps to customize
+    * Change `name` in `package.json` and `package-lock.json`
+
+* Steps to run
+    * `npm i`
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <span className="text-3xl font-bold underline">
+          this is text inside a span label with text-3xl font-bold underline
+        </span>
+      </div>
+  )
+}
+
+export default App
